9ce50677e1ebc2f9aa3dd7735e1cbeb52072196b
Samavi <alisamav1213@gmail.com>
2024-03-06 14:04:24 +0330

    feat(ui): add CustomDropdown widget with Currency selection
    Add CustomDropdown widget to provide a dropdown selection for choosing between Currency options. This widget allows users to select between Rial and Toman currencies with customizable radius for styling.

---
+++ lib/Core/Widgets/custom_dropdown.dart
@@ -0,0 +1,38 @@
+import 'package:PriceGuardian/Core/Utils/currency.dart';
+import 'package:flutter/material.dart';
+
+class CustomDropdown extends StatelessWidget {
+  final double radius;
+  final Function(Currency? value) onChanged;
+  const CustomDropdown(
+      {required this.radius, required this.onChanged, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonFormField<Currency>(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(radius),
+          borderSide: const BorderSide(color: Color(0xFF140F2D)),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(radius),
+          borderSide: const BorderSide(color: Color(0xFF140F2D)),
+        ),
+      ),
+      value: Currency.rial,
+      onChanged: onChanged,
+      items: <List>[
+        ["ریال", Currency.rial],
+        ["تومان", Currency.toman]
+      ].map<DropdownMenuItem<Currency>>((List value) {
+        return DropdownMenuItem<Currency>(
+          value: value[1],
+          child: Text(
+            value[0],
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
