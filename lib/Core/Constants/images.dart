0178ecdf59482df19b7307736d3cca15d2a257db
Samavi <alisamav1213@gmail.com>
2024-03-06 21:11:15 +0330

    feat(images): add connection image path
    Add the path for the connection image to the Images class.

--- lib/Core/Constants/images.dart
+++ lib/Core/Constants/images.dart
@@ -2,4 +2,5 @@ class Images {
   static const add = "assets/images/add.png";
   static const delete = "assets/images/delete.png";
   static const edit = "assets/images/edit.png";
+  static const connection = "assets/images/connection.png";
 }
