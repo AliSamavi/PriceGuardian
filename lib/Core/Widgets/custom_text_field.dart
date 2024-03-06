89a4fa23055d635a440cb598640f3581f19c27e0
Samavi <alisamav1213@gmail.com>
2024-03-06 12:32:38 +0330

    feat(ui): add CustomTextField widget with customizable properties
    Add CustomTextField widget to provide a customizable text input field with support for properties such as radius, fontSize, hintText, focusNode, keyboardType, onChanged, and onEditingComplete. This widget enhances the flexibility of text input customization in the user interface.

---
+++ lib/Core/Widgets/custom_text_field.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final double radius;
+  final double? fontSize;
+  final String? hintText;
+  final FocusNode? focusNode;
+  final TextInputType? keyboardType;
+  final Function(String value)? onChanged;
+  final Function()? onEditingComplete;
+  const CustomTextField(
+      {required this.controller,
+      required this.radius,
+      this.hintText,
+      this.fontSize,
+      this.focusNode,
+      this.keyboardType,
+      this.onChanged,
+      this.onEditingComplete,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      focusNode: focusNode,
+      controller: controller,
+      keyboardType: keyboardType,
+      textDirection: TextDirection.ltr,
+      cursorColor: const Color(0xFF140F2D),
+      style: TextStyle(fontSize: fontSize),
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.grey.withOpacity(0.25),
+        hintText: hintText,
+        hintTextDirection: TextDirection.ltr,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(radius),
+          borderSide: const BorderSide(color: Color(0xFF140F2D)),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(radius),
+          borderSide: const BorderSide(color: Color(0xFF140F2D)),
+        ),
+      ),
+      onChanged: onChanged,
+      onEditingComplete: onEditingComplete,
+    );
+  }
+}
